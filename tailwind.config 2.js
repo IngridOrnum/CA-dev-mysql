fdb2ecef95ba141c69779a7ca6bd702baeb6c1be
Ingrid Ornum <ingrid.ornum@gmail.com>
2025-01-26 17:19:31 +0100

    Chore: Did styling on all pages

--- tailwind.config 2.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./frontend/pages/**/*.html", "./frontend/pages/**/*.js"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./frontend/pages/**/*.html", "./frontend/pages/**/*.js"],
+  content: ["*.html", "./frontend/pages/**/*.html", "./frontend/pages/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-white': '#F6F6F6',
+        'secondary-gray': '#BFBFBF',
+        'dark-purple': '#2A0052',
+        'light-purple': '#BEA5E7',
+        'tertiary-gray': '#4D4D4D',
+        'primary-black': '#0D0D0D',
+      },
+      backgroundColor: {
+        'primary': '#0D0D0D',
+        'secondary': '#141414',
+        'tertiary': '#4D4D4D',
+        'primary-white': '#F6F6F6',
+      },
+    },
   },
   plugins: [],
 }
